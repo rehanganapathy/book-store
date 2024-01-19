873755e6e88ae805399cd7dd5b3db981fdcf1d31
Rehan Ganapathy <79349712+rehanganapathy@users.noreply.github.com>
2024-01-19 09:55:24 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -22,7 +22,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post("book-store-95c0yg7xs-rehanganapathy.vercel.app/books", data)
+      .post("https://book-store-phi-olive.vercel.app/books", data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Created successfully", { variant: "success" });
